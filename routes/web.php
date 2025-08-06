059d83b26b13ddc270722f6c86f44391804196cb
engelbertushendro10 <engelbertushendro@gmail.com>
2025-08-06 13:26:30 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,25 @@
 <?php
 
+use App\Http\Controllers\packagesController;
+use App\Livewire\App\AddPackage;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
-use App\Livewire\App\AddPackage;
-use App\Http\Controllers\packagesController;
 
 //for api
 
-Route::get('api/list/packages', [packagesController::class,'jsonData']); //livewire app to show add package view
-
-
+Route::get('api/list/packages', [packagesController::class, 'jsonData']); //livewire app to show add package view
 
 // end of api
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-
-
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
@@ -30,8 +28,8 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
-    Route::get('packages', AddPackage::class)->name('packages'); //livewire app to show add package view
+    Route::get('packages', AddPackage::class)->name('packages');   //livewire app to show add package view
     Route::post('packages.save', AddPackage::class)->name('save'); //controller to handle package saving
-    });
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
